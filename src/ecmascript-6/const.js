0dcba7133b1b779c180c5f79fe0560744947408b
Neville Bokdawalla <neville.bokdawalla@bookmyshow.com>
2018-05-23 15:40:09 +0530

    added new file demonstrating the use of 'const'

---
+++ src/ecmascript-6/const.js
@@ -0,0 +1,13 @@
+const daysOfTheWeek = [];
+
+daysOfTheWeek.push('Monday');
+daysOfTheWeek.push('Tuesday');
+daysOfTheWeek.push('Wednesday');
+daysOfTheWeek.push('Thursday');
+daysOfTheWeek.push('Friday');
+daysOfTheWeek.push('Saturday');
+daysOfTheWeek.push('Sunday');
+
+console.log(daysOfTheWeek);
+
+daysOfTheWeek = "Mon";
